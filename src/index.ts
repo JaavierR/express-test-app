617e54f96629475329ecbda48b7cd39fe25fbc30
Javier Reyes <javier.reys@gmail.com>
2022-12-12 17:21:20 -0300

    feat: download from link

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,39 @@ const PORT = 3000
 
 app.get('/', async (req, res) => {
   console.log('someone pinged me')
-  const books = []
-  const id = req.query.id
+  // const books = []
+  const id = req.query.id ?? 1
   console.log(id)
 
   const browser = await chromium.launch()
   const page = await browser.newPage()
 
-  await page.goto(`https://books.toscrape.com/catalogue/page-${id}.html`)
-  await page.screenshot({ path: 'books.png' })
+  // Also use .click() to click on the button
+  const [download] = await Promise.all([
+    page.waitForEvent('download'),
+    page
+      .goto(
+        'https://www.dailleventos.cl/wp-content/uploads/2020/08/Catalogo_Dailleventos_compressed.pdf'
+      )
+      .catch((e) => e)
+  ])
 
-  const titles = page.locator('a[title]')
+  console.log(await download.path())
+  await browser.close()
 
-  for (let i = 0; i < (await titles.count()); i++) {
-    const text = await titles.nth(i).textContent()
-    books.push({ title: text })
-  }
+  // await page.screenshot({ path: 'books1.png' })
+
+  // const titles = page.locator('a[title]')
+
+  // for (let i = 0; i < (await titles.count()); i++) {
+  //   const text = await titles.nth(i).textContent()
+  //   books.push({ title: text })
+  // }
 
   await browser.close()
 
-  res.json({ message: books })
+  // res.json({ message: books })
+  res.json({ message: 'ok' })
 })
 
 app.listen(PORT, () => {
